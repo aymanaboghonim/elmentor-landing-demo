58d8e189d80072edd5d4f7becd30070e9590eb50
aymanaboghonim <aymanaboghonim@gmail.com>
2025-11-17 23:40:20 +0200

    feat(contact): client validation, accessible errors, submit stubbed in e2e; add styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/global.css'
+import './styles/contact.css'
 import { initSmoothScroll } from './lib/smoothScroll'
 
 // Initialize smooth-scroll on client
